50098e12192ad87d56a03cbba96a176f422576cf
unknown <gmehydra@proton.me>
2024-05-23 11:17:50 -0700

    page updates

--- src/app/pages/display-timeline/timeline-controls/chapters-control/chapters.ts
+++ src/app/pages/display-timeline/timeline-controls/chapters-control/chapters.ts
@@ -18,7 +18,7 @@ export const chapterConfigs: ChapterInterface[] = [
     {
         title: 'The sneeze of 2021',
         yearGroup: '2021',
-        subtitle: 'A pile of cash, fresh leadership, new ideas',
+        subtitle: 'An unprecendent social media phenomenon has revealed opportunities and vulnerabilities.  GameStop is now run by an entirely new board of directors, and obtains a pile of cash by selling high-priced shares to the market.  GME shareholders discover DRS.',
         dateStartYYYYMMDD: '2021-01-01',
         dateEndYYYYMMDD: '2021-12-31',
         description: [],
@@ -36,7 +36,7 @@ export const chapterConfigs: ChapterInterface[] = [
     {
         title: 'Can the company become profitable?',
         yearGroup: '2023',
-        subtitle: '25% of the company is owned by direct registered shareholders, but investment momentum has dissipated.  Competing narratives vy for the truth.',
+        subtitle: '25% of the company is owned by directly registered shareholders, but investment momentum has dissipated.  Competing narratives vy for the truth.',
         dateStartYYYYMMDD: '2023-01-01',
         dateEndYYYYMMDD: '2023-12-31',
         description: [],
