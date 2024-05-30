65422796b47d8333fdaf9765411d60b28d8144a5
unknown <gmehydra@proton.me>
2024-05-29 17:10:53 -0700

    DRS

--- src/app/info-pages/drs/drs.component.ts
+++ src/app/info-pages/drs/drs.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-drs',
   templateUrl: './drs.component.html',
   styleUrls: ['./drs.component.scss']
 })
-export class DrsComponent {
+export class DrsComponent implements OnInit {
+
+  constructor(private _title: Title){
+    this._title.setTitle('DRS - Direct Registration System - gmetimeline.org');
+  }
+
+  ngOnInit(){
+
+  }
 
 }
