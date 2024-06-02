ac92eca98cfc129ae21894b8f4e2de5b76f358b6
unknown <gmehydra@proton.me>
2024-06-02 12:52:48 -0700

    update meta

--- src/app/info-pages/drs/drs.component.ts
+++ src/app/info-pages/drs/drs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-drs',
@@ -8,8 +8,11 @@ import { Title } from '@angular/platform-browser';
 })
 export class DrsComponent implements OnInit {
 
-  constructor(private _title: Title){
+  constructor(private _title: Title, private _meta: Meta){
     this._title.setTitle('DRS - Direct Registration System - gmetimeline.org');
+
+    const description = 'DRS - Direct Registration System'
+    this._meta.updateTag({ name: 'description', content: description })
   }
 
   ngOnInit(){
