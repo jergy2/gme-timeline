0b2916471ea7433d6fa9378da08898c4e502b7ba
unknown <gmehydra@proton.me>
2024-07-02 14:04:40 -0700

    DRS

--- src/app/pages/social-media/social-media.component.ts
+++ src/app/pages/social-media/social-media.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ScreenSizeService } from 'src/app/services/screen-size.service';
 
 @Component({
   selector: 'app-social-media',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./social-media.component.scss']
 })
 export class SocialMediaComponent {
-
+  constructor(private _titleService: Title, private _screenService: ScreenSizeService){
+    this._titleService.setTitle('GME Social Media');
+  }
 }
